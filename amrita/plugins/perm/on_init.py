a77cb81282f667ee7c3854afd97fbb9db87b4354
John Richard <windowserror@163.com>
2025-12-27 20:56:23 +0800

    添加自动移除功能

--- amrita/plugins/perm/on_init.py
+++ amrita/plugins/perm/on_init.py
@@ -1,3 +1,5 @@
+import shutil
+
 from nonebot import get_driver, logger
 
 from amrita.config_manager import UniConfigManager
@@ -25,8 +27,10 @@ async def load():
         dm = Data_Manager()
         store = PermissionStorage()
         await dm.init()
+        count = 0
         logger.info("Migrating permission groups...")
         for file in dm.permission_groups_path.iterdir():
+            count += 1
             if file.is_file():
                 data = PermissionGroupData.model_validate_json(file.read_text("utf-8"))
                 await store.update_permission_group(
@@ -36,6 +40,7 @@ async def load():
                 )
         logger.info("Migrating chat group data to database...")
         for file in dm.group_data_path.iterdir():
+            count += 1
             if file.is_file():
                 group_data = GroupData.model_validate_json(file.read_text("utf-8"))
                 await store.update_member_permission(
@@ -46,6 +51,7 @@ async def load():
                     )
                 )
                 for g in group_data.permission_groups:
+                    count += 1
                     logger.info(
                         f"Add member related permission group for {file.stem}@group on {g}"
                     )
@@ -57,6 +63,7 @@ async def load():
                         logger.opt(colors=True, exception=e).error(e)
         logger.info("Migrating user data to database...")
         for file in dm.user_data_path.iterdir():
+            count += 1
             if file.is_file():
                 user_data = UserData.model_validate_json(file.read_text("utf-8"))
                 await store.update_member_permission(
@@ -67,6 +74,7 @@ async def load():
                     )
                 )
                 for g in user_data.permission_groups:
+                    count += 1
                     logger.info(
                         f"Add member related permission group for {file.stem}@user on {g}"
                     )
@@ -76,7 +84,8 @@ async def load():
                         )
                     except Exception as e:
                         logger.opt(colors=True, exception=e).error(e)
-        logger.info("更新权限成功")
+        logger.info(f"更新权限成功，共{count}条数据完成迁移。")
+        shutil.rmtree(dm.plugin_data_dir)
         conf: Config = await UniConfigManager().get_config()
         conf.update_from_json = False
         await UniConfigManager().save_config()
